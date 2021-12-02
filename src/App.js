fc8b2eb341e4dd562edb05e2768222a5ac936493
Luca Boschetti <21lucboschetti@asfg.edu.mx>
2021-12-02 00:11:18 -0600

    files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useFormik} from "formik";
 
 function App() {
+  let click = false;
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      valid: '',
+    },
+    onSubmit: values => {
+      console.log('form: ', values);
+    },
+    validate: values=> {
+      let errors = {};
+      if(!values.email) errors.email = "Field required";
+      if(!values.password) errors.password = "Field required";
+      if(!values.email.includes("@") || !values.email.includes(".") ) errors.email = "Username should be an email";
+      if(Object.keys(errors).length ===0 && click===true) errors.valid = "Login Successful";
+      return errors
+    },
+
+  },);
+  let handleClick = () => {
+    click=true;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={"main"}>
+        <div className={"center"}>
+          <form onSubmit={formik.handleSubmit} className={"form"}>
+            <div>Username</div>
+            <input name="email" type="text" onChange={formik.handleChange} value={formik.values.email} placeholder={"Email"}/>
+            {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div>: null}
+            <div>Password</div>
+            <input name="password" type="text" onChange={formik.handleChange} value={formik.values.password} placeholder={"Password"}/>
+            {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div>: null}
+            <br/>
+            {formik.errors.valid ? <div style={{color: 'green'}}>{formik.errors.valid}</div>: null}
+            <button type="Submit" onClick={handleClick}>Sumbit</button>
+          </form>
+        </div>
     </div>
   );
 }
